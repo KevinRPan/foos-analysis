0f09ead874c716cb8f1608fd37e45c9b4fd58690
Kevin Pan <kevinrpan@gmail.com>
2017-03-21 19:05:17 -0700

    update
    clean up repo

--- foos_analysis/server.R
+++ foos_analysis/server.R
@@ -1,73 +1,111 @@
-
+##  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##    Server
+##  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 library(shiny)
+library(DT)
 
 shinyServer(function(input, output) {
 
-  ############# Rank table #####################################
-  output$rank_table <- DT::renderDataTable({
-    DT::datatable(singles_elo %>% mutate(elo = round(elo)), options = list(paging = FALSE), fillContainer = FALSE)
+  #### Rank table ------------------------------------------------------------
+
+  output$rank_table <- renderDataTable({
+    datatable(singles_elo %>%
+                mutate(elo = round(elo)) %>%
+                select(Players = players,
+                       Elo = elo,
+                       "Current win streak" = streak,
+                       "Max win streak" = max_streak,
+                       Wins = wins,
+                       Losses = losses,
+                       "Number of Games" = number_of_games),
+              # options = list(),
+              fillContainer = FALSE,
+              extensions = c('ColReorder', 'Buttons'),
+              options = list(paging = FALSE,
+                             dom = 'Bfrtip',
+                             buttons = I('colvis'),
+                             colReorder = TRUE)) %>%
+      formatStyle(c('Players','Elo'), fontWeight = 'Bold') %>%
+      formatStyle(
+        c('Current win streak',
+          'Max win streak'),
+        background = styleColorBar(range(singles_elo$max_streak),
+                                   'firebrick'),
+        backgroundSize = '75% 80%',
+        backgroundRepeat = 'no-repeat',
+        backgroundPosition = 'right'
+      ) %>%
+      formatStyle(
+        c('Wins',
+          'Losses',
+          'Number of Games'),
+        background = styleColorBar(range(singles_elo$number_of_games),
+                                        'steelblue'),
+        backgroundSize = '75% 80%',
+        backgroundRepeat = 'no-repeat',
+        backgroundPosition = 'right'
+      )
   })
 
-  ############# Player expectancy #####################################
-  output$player_expectancy <- renderTable(
-    player_odds_df %>% round(3) * 100,
-    rownames = TRUE,
-    digits = 1
-  )
+  #### Player expectancy ------------------------------------------------------
+
+  output$player_expectancy <- renderD3heatmap({
+    "files/singles_elo_2016.Rds" %>% readRDS %>% PlotCoWMatrix()
+  })
+
+  # renderTable(player_odds_df %>% round(3) * 100,
+  #             rownames = TRUE,
+  #             digits = 1
+  # )
 
   ############# Games table #####################################
   output$games_record <- DT::renderDataTable({
-    DT::datatable(singles_games)
+    DT::datatable(singles_games %>%
+                    select("Game Number" = game_num,
+                           Date = date,
+                           "Player 1" = t1_p1,
+                           "Player 1 Score" = t1_score,
+                           "Player 2" = t2_p1,
+                           "Player 2 Score" = t2_score)
+    )
   })
 
-  ############# New Rank plot #####################################
-  output$rank_plot <- renderPlot({
-    players_to_show <- input$playerlist
-
-    ggp <- elo_tracker %>%
-      slice(1:(num_players*(input$games+1))) %>%
-      filter(players %in% players_to_show) %>%
-      ggplot() +
-      geom_line(aes(x = game_num, y = elo, col = players, group = players)) +
-      xlab("Game Number") +
-      ylab("Elo") +
-      theme_minimal()
-
-    if(input$includePoints) {
-      ggp <- ggp +
-        geom_point(aes(x = game_num, y = elo, col = players,
-                       group = players, size = score_diff), alpha = .2)
-    }
-    ggp
+  #### 2017 Rank plot --------------------------------------------------
+  output$rank_plot_2017 <- renderPlotly({
+    elo_tracker %>% PlotElo
   })
-  ############# Old Rank plot #####################################
-  output$old_rank_plot <- renderPlot({
+  #### 2016 Rank plot --------------------------------------------------
+  output$rank_plot_2016 <- renderPlotly({
+    "files/elo_tracker_2016.Rds" %>% readRDS %>% PlotElo(input$includePoints)
+  })
+  #### Old Rank plot --------------------------------------------------
+  output$rank_plot_initial <- renderPlotly({
 
-    ### Get previous data
-    old_elo_tracker <- readRDS("../elo_tracker_first_3_months.Rds")
-    old_player_list <- old_elo_tracker %>%
-      select(players) %>%
-      distinct %>%
-      arrange(players) %>%
-      .[[1]]
+    elo_rename_f3m <- elo_tracker_f3m %>%
+      select(Game_Num = game_num,
+             Elo = elo,
+             Player = players,
+             Score_Difference = score_diff)
 
-    ### Plot it
-    ggp <- old_elo_tracker %>%
-      slice(1:(length(old_player_list)*(input$numGamesOld+1))) %>%
-      filter(players %in% input$playersToShowOld) %>%
-      ggplot() +
-      geom_line(aes(x = game_num, y = elo, col = players, group = players)) +
+    ggp <- elo_rename_f3m %>%
+      slice(1:(length(player_list_f3m)*(input$numGamesOld+1))) %>%
+      # filter(Player %in% input$playersToShowOld) %>%
+      ggplot(aes(x = Game_Num, y = Elo,
+                 col = Player)) +
+      geom_line(alpha = .8) +
       xlab("Game Number") +
       ylab("Elo") +
       theme_minimal()
 
     if(input$includePointsOld) {
       ggp <- ggp +
-        geom_point(aes(x = game_num, y = elo, col = players,
-                       group = players, size = score_diff), alpha = .2)
+        geom_point(aes(size = Score_Difference),
+                   data = elo_rename_f3m %>%
+                     # filter(Player %in% input$playersToShowOld %>%
+                     filter(Score_Difference > 0),
+                   alpha = .2)
     }
     ggp
   })
-
 })
--- foos_analysis/ui.R
+++ foos_analysis/ui.R
@@ -1,7 +1,12 @@
+##  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##    UI
+##  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 library(shiny)
-library(pacman)
-p_load(shinythemes)
+library(shinythemes)
+
+link_to_form <-
+  "https://docs.google.com/forms/d/e/1FAIpQLScpY5PEZpB6TsuTwAfg5-CZmFDcFpPcw_tdFLhI_v5U-E09hA/viewform"
 
 shinyUI(
   navbarPage(
@@ -13,15 +18,14 @@ shinyUI(
         8,
         h4("Latest SF Office foosball rankings"),
         p("So how do you rank up?"),
-        p(
-          "Add your name to the list - track your games in ",
-          tags$a(href = "https://docs.google.com/spreadsheets/d/1Zjyp6lVjJ1ADfRsK4u8OV9W9kSD7IrDHkwcy_E1iZ5I/edit?usp=sharing", "this google sheet"),
-          "to make the rankings more accurate!"
-        )
+        p("Add your name to the list - track your games with ",
+          tags$a(href = link_to_form, "this form"),
+          "to make the rankings more accurate!")
       )),
       DT::dataTableOutput("rank_table")
     ),
-    ########## Expectancies table ############################################
+
+    #### Expectancies table ----------------------------------------------------
     tabPanel(
       "Player expectancies",
       fluidRow(column(
@@ -34,108 +38,96 @@ shinyUI(
         ),
         p("Read across: Row Player's chance of beating Column Player")
       )),
-      tableOutput("player_expectancy")
+      d3heatmapOutput("player_expectancy")
     ),
 
-
-    ################ Current games #####################################
+    #### Current games ---------------------------------------------------------
     tabPanel(
       "Game records",
       fluidRow(column(
         8,
         h4("Game records"),
-        p(
-          "You can see them here or you can see them ",
-          tags$a(href = "https://docs.google.com/spreadsheets/d/1Zjyp6lVjJ1ADfRsK4u8OV9W9kSD7IrDHkwcy_E1iZ5I/edit?usp=sharing", "here"),
-          "."
-        )
+        p("Try searching for your foos-nemesis to see how they've been doing."),
+        p("Note: Doubles games are omitted.")
       )),
       DT::dataTableOutput("games_record")
     ),
 
-    navbarMenu("Rankings graphs",
+    navbarMenu(
+      "Rankings graphs",
 
-     #################### New rankings graph #################################
-     tabPanel(
-       "New rankings graph",
-       fluidRow(column(
-         8,
-         h4("Tracking player rankings over games"),
-         p(
-           "The latest and greatest, based on",
-           tags$a(href = "https://docs.google.com/spreadsheets/d/1Zjyp6lVjJ1ADfRsK4u8OV9W9kSD7IrDHkwcy_E1iZ5I/edit?usp=sharing", "your inputs"),
-           "!"
-         )
-       )),
-       sidebarLayout(
-         sidebarPanel(
-           # verbatimTextOutput('selected_players'),
-           selectInput(
-             'playerlist',
-             'Players to plot',
-             player_list,
-             selected = player_list,
-             multiple = TRUE,
-             selectize = TRUE
-           ),
-           sliderInput(
-             "games",
-             "Number of games:",
-             min = 1,
-             max = 30,
-             value = 10
-           ),
-           radioButtons("includePoints", "Include Points?",
-                        c("Yes" = TRUE, "No" = FALSE))
-         ),
-         mainPanel(plotOutput("rank_plot"))
-       )
-     ),
-      ########## Old rankings graph #############################################
+      ## 2017 rankings graph --------------------------------------------------
       tabPanel(
-        "Old rankings graph",
+        "2017 rankings graph",
         fluidRow(column(
           8,
           h4("Tracking player rankings over games"),
+          p("The latest and greatest, based on",
+            tags$a(href = link_to_form, "your inputs"),"!")
+        )),
+        plotlyOutput("rank_plot_2017")
+      ),
+      ## 2016 rankings graph --------------------------------------------------
+      tabPanel(
+        "2016 rankings graph",
+        fluidRow(column(
+          8,
+          h4("Tracking player rankings over games"),
+          p("How'd 2016 go?")
+        )),
+        sidebarLayout(
+          sidebarPanel(
+            radioButtons("includePoints", "Include Points?",
+                         c("Yes" = TRUE, "No" = FALSE)),
+            width = 2
+          ),
+          mainPanel(
+            plotlyOutput("rank_plot_2016")))
+        ),
+      ## Initial rankings graph --------------------------------------------------
+      tabPanel(
+        "Initial rankings graph",
+        fluidRow(column(
+          8,
+          h4("Tracking player rankings over games"),
+          p("Try pressing the play button!"),
           p(
             "Don't you wanna see your line move? Update ",
-            tags$a(href = "https://docs.google.com/spreadsheets/d/1Zjyp6lVjJ1ADfRsK4u8OV9W9kSD7IrDHkwcy_E1iZ5I/edit?usp=sharing", "that tracker"),
-            "
-            for sweet graphing glory!"
+            tags$a(href = link_to_form, "that tracker"),
+            "for sweet graphing glory!"
           )
           )),
         sidebarLayout(
           sidebarPanel(
-            # verbatimTextOutput('old_selected_players'),
-            selectInput(
-              'playersToShowOld',
-              'Players to plot',
-              old_player_list,
-              selected = old_player_list,
-              multiple = TRUE,
-              selectize = TRUE
-            ),
+            # selectInput(
+            #   'playersToShowOld',
+            #   'Players to plot',
+            #   player_list_f3m,
+            #   selected = player_list_f3m,
+            #   multiple = TRUE,
+            #   selectize = TRUE
+            # ),
             sliderInput(
               "numGamesOld",
               "Number of games:",
               min = 1,
-              max = 60,
-              value = 30
+              max = nrow(singles_games_f3m),
+              value = nrow(singles_games_f3m),
+              step = 1,
+              animate = animationOptions(interval = 250, loop = FALSE)
             ),
             radioButtons(
               "includePointsOld",
               "Include Points?",
               c("Yes" = TRUE, "No" = FALSE)
-            )
+            ),
+            width = 2
           ),
-          mainPanel(plotOutput("old_rank_plot"))
+          mainPanel(plotlyOutput("rank_plot_initial"))
         )
-        )
-     ),
-
+      )),
 
     tabPanel("About Elo",
-             includeMarkdown("elo.md"))
+             includeMarkdown("files/elo.md"))
 
-)
-)
+  ))
